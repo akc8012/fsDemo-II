2211ee0c3bc6c0a163493ed6bd7082f7722cffa0
Andrew Colannino <akc8012@gmail.com>
2019-09-15 22:00:13 -0400

    All spaces must be BIG BOLD BEAUTIFUL TABBI BOIS

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -4,22 +4,22 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
-    public bool active;
-    public GameObject gameCamera;
-    public GameObject extraCamera;
+	public bool active;
+	public GameObject gameCamera;
+	public GameObject extraCamera;
 
-    void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.Alpha1))
-        {
-            active = !active;
-            GameCamera(active);
-        }
-    }
+	void Update()
+	{
+		if (Input.GetKeyDown(KeyCode.Alpha1))
+		{
+			active = !active;
+			GameCamera(active);
+		}
+	}
 
-    void GameCamera(bool state)
-    {
-        gameCamera.SetActive(!state);
-        extraCamera.SetActive(state);
-    }
+	void GameCamera(bool state)
+	{
+		gameCamera.SetActive(!state);
+		extraCamera.SetActive(state);
+	}
 }
--- Assets/Scripts/RingScript.cs
+++ Assets/Scripts/RingScript.cs
@@ -5,29 +5,29 @@ using DG.Tweening;
 
 public class RingScript : MonoBehaviour
 {
-    public float speed;
-    public bool activated;
+	public float speed;
+	public bool activated;
 
-    // Update is called once per frame
-    void Update()
-    {
-        if(!activated)
-            transform.eulerAngles += new Vector3(0, speed, 0) * Time.deltaTime;
-    }
+	// Update is called once per frame
+	void Update()
+	{
+		if(!activated)
+			transform.eulerAngles += new Vector3(0, speed, 0) * Time.deltaTime;
+	}
 
-    private void OnTriggerEnter(Collider other)
-    {
-        if (other.CompareTag("Player"))
-        {
-            activated = true;
-            transform.parent = other.transform.parent;
+	private void OnTriggerEnter(Collider other)
+	{
+		if (other.CompareTag("Player"))
+		{
+			activated = true;
+			transform.parent = other.transform.parent;
 
-            Sequence s = DOTween.Sequence();
+			Sequence s = DOTween.Sequence();
 
-            s.Append(transform.DORotate(Vector3.zero, .2f));
-            s.Append(transform.DORotate(new Vector3(0, 0, -900), 3, RotateMode.LocalAxisAdd));
-            s.Join(transform.DOScale(0, .5f).SetDelay(1f));
-            s.AppendCallback(() => Destroy(gameObject));
-        }
-    }
+			s.Append(transform.DORotate(Vector3.zero, .2f));
+			s.Append(transform.DORotate(new Vector3(0, 0, -900), 3, RotateMode.LocalAxisAdd));
+			s.Join(transform.DOScale(0, .5f).SetDelay(1f));
+			s.AppendCallback(() => Destroy(gameObject));
+		}
+	}
 }
