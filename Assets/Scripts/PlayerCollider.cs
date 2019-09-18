a27d1c2450cba9f3524ac2dcad3d47d104d940e3
Andrew Colannino <akc8012@gmail.com>
2019-09-17 20:45:10 -0400

    Created PlayerCollider, it resets along track position when hit

---
+++ Assets/Scripts/PlayerCollider.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+public class PlayerCollider : MonoBehaviour
+{
+	[SerializeField]
+	CinemachineDollyCart CinemachineDollyCart;
+
+    void OnTriggerEnter(Collider other) => ResetCartPosition();
+
+    // TODO: This should be done in a different script
+    void ResetCartPosition() => CinemachineDollyCart.m_Position = 0;
+}
