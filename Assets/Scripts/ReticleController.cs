ebe88f5d0011375170e7d7e7daeecdc39c00a91b
Andrew Colannino <akcolannino@email.neit.edu>
2020-09-30 23:58:28 -0400

    Cusor-controlled reticle is a go

---
+++ Assets/Scripts/ReticleController.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class ReticleController : MonoBehaviour
+{
+	[SerializeField]
+	private Graphic Reticle = null;
+
+	[SerializeField]
+	private bool Lerp = true;
+
+	[SerializeField]
+	private float LerpAdjust = 1;
+
+	void Awake()
+	{
+		Cursor.visible = false;
+	}
+
+	void Update()
+	{
+		if (Lerp)
+			Reticle.rectTransform.position = Vector3.Lerp(Reticle.rectTransform.position, Input.mousePosition, Time.deltaTime * LerpAdjust);
+		else
+			Reticle.rectTransform.position = Input.mousePosition;
+
+	}
+}
