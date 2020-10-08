9cdd6119c7b6f6c7ecb03223bc37093e541c95b1
Andrew Colannino <akcolannino@email.neit.edu>
2020-10-08 00:02:27 -0400

    ColorfulPipeChunk randomly picks a colorful material

---
+++ Assets/Scripts/RandomMaterialPicker.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class RandomMaterialPicker : MonoBehaviour
+{
+	[SerializeField]
+	private Material[] Materials;
+
+	void Start()
+	{
+		var renderer = GetComponent<Renderer>();
+		renderer.material = Materials[Random.Range(0, Materials.Length)];
+	}
+}
