08016c49fa4a1b2cc5396bdafa01ceb0b93f75ba
Andrew Colannino <akcolannino@email.neit.edu>
2020-10-08 00:34:20 -0400

    ColorfulPipeGenerator generates colorful pipes

---
+++ Assets/Scripts/ColorfulPipeGenerator.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class ColorfulPipeGenerator : MonoBehaviour
+{
+	[SerializeField] GameObject ColorfulPipe = null;
+	[SerializeField] int Amount = 50;
+	[SerializeField] Vector3 SpawnOffset = Vector3.zero;
+
+	void Start()
+	{
+		Vector3 position = transform.position;
+		for (int i = 0; i < Amount; i++)
+		{
+			Instantiate(ColorfulPipe, position, Quaternion.identity);
+			position += SpawnOffset;
+		}
+	}
+}
