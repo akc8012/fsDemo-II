0e21b079704065218ab7ee381e385297c25ea5f3
Andrew Colannino <akcolannino@email.neit.edu>
2020-10-08 01:34:25 -0400

    Rotation randomize for Colorful pipe chunks, new random materials

--- Assets/Scripts/Rotation.cs
+++ Assets/Scripts/Rotation.cs
@@ -2,11 +2,28 @@
 
 public class Rotation : MonoBehaviour
 {
-	[SerializeField]
-	float Speed = 25;
+	[SerializeField] float Speed = 25;
+	[SerializeField] Vector3 Axis = new Vector3(0, 0, 1);
+	[SerializeField] bool Randomize = false;
+
+	const float MaxSpeed = 125;
+	const float MinSpeed = 60;
+
+	void Start()
+	{
+		if (Randomize)
+		{
+			do
+				Speed = Random.Range(-MaxSpeed, MaxSpeed);
+			while (Mathf.Abs(Speed) < MinSpeed);
+
+			print(Speed);
+		}
+	}
 
 	void Update()
 	{
-		transform.Rotate(new Vector3(0, 0, Speed * Time.deltaTime));
+		Vector3 rotation = Axis * (Speed * Time.deltaTime);
+		transform.Rotate(rotation);
 	}
 }
