5e1beb40adf59fe4e6dddebed704858e1b29fc74
Andrew Colannino <akcolannino@email.neit.edu>
2020-10-07 21:53:53 -0400

    Rotating cube obstacle

---
+++ Assets/Scripts/Rotation.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class Rotation : MonoBehaviour
+{
+	[SerializeField]
+	float Speed = 25;
+
+	void Update()
+	{
+		transform.Rotate(new Vector3(0, 0, Speed * Time.deltaTime));
+	}
+}
